1a0fe6c06a990047a1864997fa486e698f3d191a
superuser <whiskey@benkhaled.com>
2025-03-11 02:00:02 +0100

    initial commit: Add file structure

---
+++ views/main.view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html >
+<html lang="en">
+  <head>
+    <title>Pomodoro timer</title>
+    <meta charset="utf-8">
+
+    <!-- stylesheets -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- scripts -->
+    <script src="js/script.js" defer></script>
+  </head>
+  <body>
+    
+  </body>
+</html>
